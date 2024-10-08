4c15f04cd469836d8fa622bbc619aa9b2aaaa86c
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2024-10-08 19:41:42 +0700

    commit

--- Selection2Exp1244107060084.java
+++ Selection2Exp1244107060084.java
@@ -8,8 +8,8 @@ public class Selection2Exp1244107060084 {
     System.out.print("Input year: ");
     int year = input.nextInt();
 
-    if (year % 4 == 0)  
-      if (year % 100 != 0) 
+    if (year % 4 == 0)  // Check divisibility by 4 first
+      if (year % 100 != 0) // Check divisibility by 100 only if divisible by 4
         System.out.println("Leap year");
 
     else

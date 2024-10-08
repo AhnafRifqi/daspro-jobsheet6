01a30126cd1f26ab3320ec79b05ffca53dd89dc6
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2024-10-08 19:34:06 +0700

    commit

---
+++ Selection2Exp1244107060084.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Selection2Exp1244107060084 {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Input year: ");
+    int year = input.nextInt();
+
+    if (year % 4 == 0)  
+      if (year % 100 != 0) 
+        System.out.println("Leap year");
+
+    else
+      System.out.println("Not a Leap year");
+  }
+}

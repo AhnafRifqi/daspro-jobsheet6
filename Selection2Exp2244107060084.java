8f14a641f696fba3a25fafc0a9580a33c0ebc783
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2024-10-09 21:53:51 +0700

    types of triangle

--- Selection2Exp2244107060084.java
+++ Selection2Exp2244107060084.java
@@ -5,7 +5,6 @@ public class Selection2Exp2244107060084 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in); 
 
-    
     System.out.print("Enter angle 1: ");
     double angle1 = input.nextDouble();
     System.out.print("Enter angle 2: ");
@@ -13,20 +12,22 @@ public class Selection2Exp2244107060084 {
     System.out.print("Enter angle 3: ");
     double angle3 = input.nextDouble();
 
-    
+   
     double totalAngle = angle1 + angle2 + angle3;
 
-   
+    
     if (totalAngle == 180) 
       if (angle1 == 90 || angle2 == 90 || angle3 == 90) 
         System.out.println("Right triangle");
+       else if (angle1 == angle2 && angle2 == angle3) 
+        System.out.println("Equilateral triangle");
+       else if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3) 
+        System.out.println("Isosceles triangle");
        else 
-        System.out.println("Not a right triangle");
+        System.out.println("Scalene triangle");
       
      else 
       System.out.println("Not a triangle");
     
-
-      
   }
 }

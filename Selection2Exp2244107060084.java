01d6e2ec5b5c2df1367c69f7fcd1fd84fac8ae1a
AhnafRifqi <ahnaf.rifqi1412@gmail.com>
2024-10-09 21:47:12 +0700

    triangle

---
+++ Selection2Exp2244107060084.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Selection2Exp2244107060084 {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in); 
+
+    
+    System.out.print("Enter angle 1: ");
+    double angle1 = input.nextDouble();
+    System.out.print("Enter angle 2: ");
+    double angle2 = input.nextDouble();
+    System.out.print("Enter angle 3: ");
+    double angle3 = input.nextDouble();
+
+    
+    double totalAngle = angle1 + angle2 + angle3;
+
+   
+    if (totalAngle == 180) 
+      if (angle1 == 90 || angle2 == 90 || angle3 == 90) 
+        System.out.println("Right triangle");
+       else 
+        System.out.println("Not a right triangle");
+      
+     else 
+      System.out.println("Not a triangle");
+    
+
+      
+  }
+}
